8e263a1e41e98004b2df1ce7112a515ff8979873
TheCCplanet <the.cc.planet@gmail.com>
2025-11-10 21:12:37 +0330

    x25510 curve shared key generator

--- internal/crypto/dh.go
+++ internal/crypto/dh.go
@@ -8,10 +8,43 @@ import (
 )
 
 func GenerateX25519KeyPair() (string, string, error) {
-	privateKey, err := ecdh.X25519().GenerateKey(rand.Reader)
+	curve := ecdh.X25519()
+
+	privateKey, err := curve.GenerateKey(rand.Reader)
 	if err != nil {
 		return "", "", fmt.Errorf("failed to generate private key: %w", err)
 	}
 	publicKey := privateKey.PublicKey()
 	return base64.StdEncoding.EncodeToString(publicKey.Bytes()), base64.StdEncoding.EncodeToString(privateKey.Bytes()), nil
+}
+
+func GenerateX25519SharedKey(publicKey string, privateKey string) (string, error) {
+	curve := ecdh.X25519()
+
+	publicKeyBytes, err := base64.StdEncoding.DecodeString(publicKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode public key: %w", err)
+	}
+
+	privateKeyBytes, err := base64.StdEncoding.DecodeString(privateKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode private key: %w", err)
+	}
+
+	privKey, err := curve.NewPrivateKey(privateKeyBytes)
+	if err != nil {
+		return "", fmt.Errorf("failed to import private key: %w", err)
+	}
+
+	pubKey, err := curve.NewPublicKey(publicKeyBytes)
+	if err != nil {
+		return "", fmt.Errorf("failed to import private key: %w", err)
+	}
+
+	sharedKey, err := privKey.ECDH(pubKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate shared key: %w", err)
+	}
+
+	return base64.StdEncoding.EncodeToString(sharedKey), nil
 }
